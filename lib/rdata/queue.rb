233e94271a04ecec614f789b49f1ebd8ad64a8d7
Markus Hauck <markus1189@gmail.com>
2012-12-03 19:38:29 +0100

    Chande indentation to 2

--- lib/rdata/queue.rb
+++ lib/rdata/queue.rb
@@ -1,23 +1,23 @@
 module RData
-    
-    class Queue
 
-        def initialize
-            @queue = Array[]
-            @head, @tail = 0
-        end
+  class Queue
 
-        def enqueue(x)
-            @queue[@tail] = x
-            @tail = (@tail == @length) ? 1 : @tail - 1
-        end
-
-        def dequeue
-            x = @head
-            @head = (@head == @length) ? 1 : @head + 1
-            return x
-        end        
-
+    def initialize
+      @queue = Array[]
+      @head, @tail = 0
     end
+
+    def enqueue(x)
+      @queue[@tail] = x
+      @tail = (@tail == @length) ? 1 : @tail - 1
+    end
+
+    def dequeue
+      x = @head
+      @head = (@head == @length) ? 1 : @head + 1
+      return x
+    end
+
+  end
 
 end
--- lib/rdata/stack.rb
+++ lib/rdata/stack.rb
@@ -1,44 +1,44 @@
 =begin
-    
+
 This is the Stack class. It allows easy implementation of stacks and use of their operations
 
 # @stack = RData.Stack
 # @stack.operation
 # ... etc ...
-    
+
 =end
 
 module RData
-    
-    class Stack
 
-        def initialize
-            @stack = Array[]
-            @top = 0
-        end
+  class Stack
 
-        def top
-            return @stack[@top]
-        end
-
-        def push(x)
-            @top = @top + 1
-            @stack[@top] = x
-        end
-
-        def pop
-            if self.is_empty? == "true"
-                raise '[underflow] Cannot pop data from an empty stack'
-            else
-                @top = @top - 1
-                return @stack[@top + 1]
-            end 
-        end
-
-        def is_empty?
-            (@top == 0) ? 'true' : 'false'
-        end
-
+    def initialize
+      @stack = Array[]
+      @top = 0
     end
+
+    def top
+      return @stack[@top]
+    end
+
+    def push(x)
+      @top = @top + 1
+      @stack[@top] = x
+    end
+
+    def pop
+      if self.is_empty? == "true"
+        raise '[underflow] Cannot pop data from an empty stack'
+      else
+        @top = @top - 1
+        return @stack[@top + 1]
+      end
+    end
+
+    def is_empty?
+      (@top == 0) ? 'true' : 'false'
+    end
+
+  end
 
 end
